111427ddfd673b8afa842af9bdae1d4aac811056
Diptamoy Mitra <91617575+Diptamoy-Mitra@users.noreply.github.com>
2022-05-03 17:52:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function val(result) {
+    form.disp.value = form.disp.value + result;
+}
+
+function calculate() {
+    if (form.disp.value == "") {
+        alert("Please enter numbers");
+    } else {
+        form.disp.value = eval(form.disp.value);
+    }
+}
+var btn = form.veql;
+btn.addEventListener('dblclick', function() {
+    form.disp.value = "";
+
+    // when we double click on equal button the result will be clear
+
+
+});
